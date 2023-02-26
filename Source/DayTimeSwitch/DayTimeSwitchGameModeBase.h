87ec88cba275370201bc6b9ee16e4bd9c7d56578
Toofan Tahmouresi <toofan.tah@gmail.com>
2023-02-26 14:28:16 +0300

    Init Commit

---
+++ Source/DayTimeSwitch/DayTimeSwitchGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DayTimeSwitchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAYTIMESWITCH_API ADayTimeSwitchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
